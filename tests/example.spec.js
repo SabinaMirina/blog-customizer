64dc704a1be7812a38a109bc143f522ef4933687
Sabina Mirina <mirinasabina@mail.ru>
2024-12-09 12:38:39 +0300

    fix5

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,9 @@ const { test, expect } = require('@playwright/test');
 
 test('basic test', async ({ page }) => {
 	await page.goto('https://playwright.dev/');
-	expect(await page.title()).toBe(
+	const title = await page.title();
+	console.log(`Page title is: ${title}`);
+	expect(title).toBe(
 		'Fast and reliable end-to-end testing for modern web apps | Playwright'
 	);
 });
