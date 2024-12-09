d0fdd72558e4fe82e4331e631b01e59e650a8600
Sabina Mirina <mirinasabina@mail.ru>
2024-12-09 09:04:26 +0300

    1check

--- src/ui/arrow-button/index.tsx
+++ src/ui/arrow-button/index.tsx
@@ -1,1 +1,20 @@
+import React, { useState } from 'react';
 export { ArrowButton } from './ArrowButton';
+import { ArrowButton } from './ArrowButton';
+
+export const FormToggle = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	// Функция обработки клика
+	const handleToggle = () => {
+		setIsOpen((prev) => !prev); // Изменение состояния (открыть/закрыть)
+	};
+
+	return (
+		<div>
+			{/* Передаем состояние и обработчик клика в ArrowButton */}
+			<ArrowButton isOpen={isOpen} onClick={handleToggle} />
+			{isOpen && <div>Форма открыта!</div>}
+		</div>
+	);
+};
